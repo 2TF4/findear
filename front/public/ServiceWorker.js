092e848158d24ce4ca2c2a8ee1c180daa0237f59
Ji Insung <49591292+JIINSUNG@users.noreply.github.com>
2025-03-25 02:55:24 +0900

    refactor : 서비스 워커 캐싱 로직 추가

--- front/public/ServiceWorker.js
+++ front/public/ServiceWorker.js
@@ -1,20 +1,61 @@
 let cacheData = "appV1";
 
-// 서비스 워커 설치
+/**
+*  서비스 워커 설치
+*/
 self.addEventListener("install", (event) => {
-  console.log("install", event);
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/app/index.css",
+      ]);
+    })
+  );
 });
 
-// 서비스 워커 활성화
-self.addEventListener("activate", function () {
-  console.log("fcm sw activate..");
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== cacheData) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener("push", (event) => {
   const data = event.data.json();
-  console.log("push data", data);
 
   self.registration.showNotification(data.title, {
     body: data.message,
   });
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then((fetchResponse) => {
+        if (!fetchResponse || fetchResponse.status !== 200 || fetchResponse.type !== 'basic') {
+          return fetchResponse;
+        }
+
+        const responseToCache = fetchResponse.clone();
+        caches.open(cacheData).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return fetchResponse;
+      });
+    })
+  );
 });
